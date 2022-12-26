5126f00d9dea987254504645abce07d3554b65ea
Jacques Marneweck <jacques@siberia.co.za>
2022-12-26 22:24:54 +0200

    Prep to test

---
+++ tests/bootstrap.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+/**
+ * Gotrue client for PHP
+ *
+ * @author    Jacques Marneweck <jacques@siberia.co.za>
+ * @copyright 2022 Jacques Marneweck.  All rights strictly reserved.
+ */
+
+require_once __DIR__ . '/../vendor/autoload.php';
